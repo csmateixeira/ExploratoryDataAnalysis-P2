f4edd7831c04d449e4f87b9c35dc5bd6ab8c6e01
csmateixeira <csma.teixeira@gmail.com>
2014-10-25 13:40:20 +0100

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,21 @@
+library(plyr)
+library(dplyr)
+
+if(!file.exists("data/NEI_data.zip")){
+    download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = "data/NEI_data.zip", method = "curl")
+    unzip("data/NEI_data.zip", exdir = "data")
+}
+
+NEI <- readRDS("data/summarySCC_PM25.rds")
+
+png(filename = "plot2.png")
+
+with(
+    NEI %>% 
+        filter(fips == "24510") %>%
+        group_by(year) %>% 
+        summarize(total = sum(Emissions)),        
+    plot(year, total, type = "l", xlab = "Year", ylab = "PM2.5", main = "Total PM2.5 In Baltimore City, Maryland (All Sources)")
+)
+
+dev.off()
